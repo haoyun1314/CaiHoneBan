ac7fe0a20ff848e22e0c13a21dc8ceedfc81a743
fandongdong <fandongdong@yixia.com>
2017-11-22 17:17:22 +0800

    CaiHoneBan

---
+++ Podfile
@@ -0,0 +1,10 @@
+platform:ios,'8.0'
+use_frameworks!
+
+target 'rainbowClass' do
+ 
+pod 'Masonry'
+pod 'AFNetworking'
+pod 'SDWebImage'
+
+end
---
+++ rainbowClass/Resources/CoreAnimation/MPTCoreAnimationViewController.h
@@ -0,0 +1,12 @@
+//
+//  MPTCoreAnimationViewController.h
+//  rainbowClass
+//
+//  Created by 范浩云 on 2017/11/3.
+//Copyright © 2017年 guoyun. All rights reserved.
+//
+
+@interface MPTCoreAnimationViewController : UIViewController
+
+
+@end
